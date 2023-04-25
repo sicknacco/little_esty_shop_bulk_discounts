614738f6fffddffd5642c4213d72dbde3bd6fd6f
Nick Sacco <nicksacco10@gmail.com>
2023-04-25 16:02:07 -0700

    test: bulk discount tests

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe 'Bulk Discount Index Page', type: :feature do
     @discount2 = BulkDiscount.create!(name: "Discount 2", threshold: 10, discount: 10, merchant_id: @merchant1.id)
     @discount3 = BulkDiscount.create!(name: "Discount 3", threshold: 30, discount: 30, merchant_id: @merchant1.id)
   end
+
+  describe "I see all of my bulk discounts and their attributes" do
+    it 'shows bulk discounts and their attributes' do
+      visit merchant_bulk_discounts_path(@merchant1)
+
+      within "#discount_#{@discount1.id}" do
+        expect(page).to have_link(@discount1.name)
+        expect(page).to have_content("Discount Percentage: #{@discount1.discount}")
+        expect(page).to have_content("Number of items for discount: #{@discount1.threshold}")
+
+        expect(page).to_not have_link(@discount2.name)
+        expect(page).to_not have_content("Discount Percentage: #{@discount2.discount}")
+        expect(page).to_not have_content("Number of items for discount: #{@discount2.threshold}")
+      end
+    end
+  end
 end
